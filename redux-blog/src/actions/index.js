d0ada859d21cb9d15ac20bcc85b9dd337871f306
Mike VanSleen <mike.vansleen@gmail.com>
2019-07-07 12:32:59 -0600

    Added thunk middleware to project

---
+++ redux-blog/src/actions/index.js
@@ -0,0 +1,12 @@
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+const FETCH_POSTS = "FETCH_POSTS";
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get("/posts");
+
+  dispatch({
+    type: FETCH_POSTS,
+    payload: response
+  });
+};
